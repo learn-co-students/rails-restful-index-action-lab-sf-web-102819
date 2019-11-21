9943b6f8effa3d1f2ba224ec4f0a6f92b3483b23
aryaziai <arya721@gmail.com>
2019-11-21 13:42:11 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ActiveRecord::Base
+  # attr_accessor :first_name, :last_name 
+
+
   def to_s
     self.first_name + " " + self.last_name
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Student.create([{ first_name: "Rick", last_name: "Sanchez" }, { first_name: "Morty", last_name: "Smith" }])
+Student.create(first_name: "Rick", last_name: "Sanchez")
+Student.create(first_name: "Morty", last_name: "Smith")
+Student.create(first_name: "JOEY", last_name: "GONZALES")
